920f65f0f8367ed40bc7642f58460ce00751c67c
ayana <ayanaamwayi@gmail.com>
2024-10-05 21:46:04 +0300

    added the insert data into database functionality in display_users.php

--- display_users.php
+++ display_users.php
@@ -2,22 +2,50 @@
 global $conn;
 require 'db.php';
 
-class User {
+class User
+{
     private $username;
     private $email;
     private $password;
 
-    public function __construct($username, $email, $password) {
+    public function __construct($username, $email, $password)
+    {
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
     }
-    public function getName() {
+
+    public function getName()
+    {
         return $this->username;
     }
-    public function getEmail() {
+
+    public function getEmail()
+    {
         return $this->email;
     }
-    public function getPassword() {
+
+    public function getPassword()
+    {
         return $this->password;
     }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$username = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+    // Insert the data into the database
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+if (mysqli_query($conn, $sql)) {
+echo " ";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$user = new User($username, $email, $password);
+} else {
+    echo "Form data not submitted";
+}
+?>
