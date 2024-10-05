99357bd7b09ce689170158297b0146fd16c6a6a8
ayana <ayanaamwayi@gmail.com>
2024-10-05 21:42:07 +0300

    added function construct in the display_users.php class

--- display_users.php
+++ display_users.php
@@ -1,1 +1,14 @@
 <?php
+global $conn;
+require 'db.php';
+
+class User {
+    private $username;
+    private $email;
+    private $password;
+
+    public function __construct($username, $email, $password) {
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+    }
